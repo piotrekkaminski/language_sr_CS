f65305563f46eb087695de35455a5d5c8de79a2f
Piotr Kaminski <magentopeter@gmail.com>
2017-08-25 15:04:13 -0500

    Translation autoupdate from Crowdin for sr_CS - 2017-08-25 20:04:13 branch Head

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magentotwotranslations_sr_cs',
+    __DIR__
+);
